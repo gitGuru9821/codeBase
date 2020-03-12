ba3c550a38ced5dab8ad5aab51f38f109c264850
gitGuru9821 <62069094+gitGuru9821@users.noreply.github.com>
2020-03-12 15:28:29 +0530

    Add files via upload

---
+++ src/main/java/com/gG/GG/GgApplication.java
@@ -0,0 +1,13 @@
+package com.gG.GG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gG/GG/GgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gG.GG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
